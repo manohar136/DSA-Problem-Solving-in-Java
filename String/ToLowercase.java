236e74931ab31ee8a1766a2cb77ce4aa44761620
manohar136 <chmanu2006@gmail.com>
2026-09-23 22:26:11 +0530

    Create ToLowercase.java

---
+++ String/ToLowercase.java
@@ -0,0 +1,19 @@
+// LeetCode 709 - To Lower Case
+// Approach:
+// 1. Take the given string.
+// 2. Convert it into lowercase using toLowerCase().
+// 3. Return the converted string.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+    public String toLowerCase(String s) {
+        
+        // Create a new string after converting to lowercase
+        String t = s.toLowerCase();
+        
+        // Return the result
+        return t;
+    }
+}
+
